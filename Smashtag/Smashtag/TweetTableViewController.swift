f8b34ddab6378c5adf6eac45f22c274746dadaf9
kawnayeen <kawnayeen.08@gmail.com>
2018-01-12 16:29:24 +0600

    Smashtag project created

---
+++ Smashtag/Smashtag/TweetTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TweetTableViewController.swift
+//  Smashtag
+//
+//  Created by Kawnayeen on 1/12/18.
+//  Copyright © 2018 Kawnayeen. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewController: UITableViewController {
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+}
