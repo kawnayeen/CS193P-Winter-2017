24e3615da97fec37174681b94f15caacfe6d66d2
kawnayeen <kawnayeen.08@gmail.com>
2018-01-16 13:12:38 +0600

    Custom Table view cell developed to show tweet

---
+++ Smashtag/Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by Kawnayeen on 1/16/18.
+//  Copyright © 2018 Kawnayeen. All rights reserved.
+//
+
+import UIKit
+import Twitter
+
+class TweetTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var tweetProfileImageView: UIImageView!
+    @IBOutlet weak var tweetedTimeLabel: UILabel!
+    @IBOutlet weak var tweetUserLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    
+    var tweet: Twitter.Tweet? {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    private func updateUI() {
+        tweetTextLabel?.text = tweet?.text
+        tweetUserLabel?.text = tweet?.user.description
+        
+        if let profileImageURL = tweet?.user.profileImageURL {
+            if let imageData = try? Data(contentsOf: profileImageURL) {
+                tweetProfileImageView?.image = UIImage(data: imageData)
+            }
+        } else {
+            tweetProfileImageView?.image = nil
+        }
+        
+        if let created = tweet?.created {
+            let formatter = DateFormatter()
+            if Date().timeIntervalSince(created) > 24*60*60 {
+                formatter.dateStyle = .short
+            } else {
+                formatter.timeStyle = .short
+            }
+            tweetedTimeLabel?.text = formatter.string(from: created)
+        }else{
+            tweetedTimeLabel?.text = nil
+        }
+    }
+}
