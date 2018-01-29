fca454c882cdc32a38c01a735ee0a14fb9fd555f
kawnayeen <kawnayeen.08@gmail.com>
2018-01-29 11:53:57 +0600

    Image is loading from URL

---
+++ Cassini/Cassini/DemoURL.swift
@@ -0,0 +1,13 @@
+//
+//  DemoURL.swift
+//  Cassini
+//
+//  Created by Kawnayeen on 1/29/18.
+//  Copyright © 2018 Kawnayeen. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL {
+    static let standford = URL(string: "https://s3-us-west-1.amazonaws.com/stanford.ucomm.newsms.media/wp-content/uploads/2016/03/30160018/Stanford_News_hero_image.jpg")
+}
---
+++ Cassini/Cassini/ImageViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Kawnayeen on 1/29/18.
+//  Copyright © 2018 Kawnayeen. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    var imageURL : URL? {
+        didSet {
+            image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(imageView)
+        imageURL = DemoURL.standford
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if image == nil {
+            fetchImage()
+        }
+    }
+    
+    private var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+}
